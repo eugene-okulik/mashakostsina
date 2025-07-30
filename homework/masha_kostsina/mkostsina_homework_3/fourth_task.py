ef39722c14a9625a66e1973810f8b067571290ad
MashaKostsina <kostsinamaryia@gmail.com>
2025-07-30 20:28:49 +0300

    all files were updated

--- homework/masha_kostsina/mkostsina_homework_3/fourth_task.py
+++ homework/masha_kostsina/mkostsina_homework_3/fourth_task.py
@@ -1,7 +1,7 @@
 
 f_leg = 3
 s_leg = 4
-hypotenuse = pow((f_leg ** 2 + s_leg ** 2), 1/2)
+hypotenuse = pow((f_leg ** 2 + s_leg ** 2), 1 / 2)
 area = (f_leg * s_leg) / 2
 
 print(f"Гипотенуза: {hypotenuse}")
