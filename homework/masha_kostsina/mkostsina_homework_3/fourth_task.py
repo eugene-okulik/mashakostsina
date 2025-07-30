66a482c211d3d8179c9688ab561cd2b29fe65543
MashaKostsina <kostsinamaryia@gmail.com>
2025-07-30 18:39:38 +0300

    updated 4 files

--- homework/masha_kostsina/mkostsina_homework_3/fourth_task.py
+++ homework/masha_kostsina/mkostsina_homework_3/fourth_task.py
@@ -4,3 +4,11 @@ hypotenuse = pow((f_leg**2 + s_leg**2), 1/2)
 
 print(f"Гипотенуза:", hypotenuse )
 print(f'Площадь:', (f_leg*s_leg)/2)
+
+
+
+
+
+
+
+
--- homework/masha_kostsina/mkostsina_homework_3/third_task.py
+++ homework/masha_kostsina/mkostsina_homework_3/third_task.py
@@ -3,3 +3,4 @@ s_number = 2
 
 print(f'Среднее арифметическое:', (f_number + s_number)/2)
 print(f'Среднее геометрическое:', pow(f_number * s_number, 1/2))
+
