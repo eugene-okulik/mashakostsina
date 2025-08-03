a4106f8a847dba2385a10d118c897217e5748d3c
MashaKostsina <kostsinamaryia@gmail.com>
2025-08-03 10:23:44 +0300

    homework_7 is done

---
+++ homework/masha_kostsina/mkostsina_homework_7/homework_7_task1.py
@@ -0,0 +1,10 @@
+
+number = 6
+
+while True:
+    user_input = int(input())
+    if user_input == number:
+        print("Поздравляю! Вы угадали!")
+        break
+    else:
+        print("попробуйте снова")
---
+++ homework/masha_kostsina/mkostsina_homework_7/homework_7_task3.py
@@ -0,0 +1,4 @@
+
+user_input = input().split()
+
+print(int(user_input[-1]) + 10)
