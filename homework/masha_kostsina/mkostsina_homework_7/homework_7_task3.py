d88b47fe31a2b39f19cd0cb5fd2b07f0150d89ed
MashaKostsina <kostsinamaryia@gmail.com>
2025-08-03 10:40:57 +0300

    task 3 is updated

--- homework/masha_kostsina/mkostsina_homework_7/homework_7_task3.py
+++ homework/masha_kostsina/mkostsina_homework_7/homework_7_task3.py
@@ -1,4 +1,14 @@
 
-user_input = input().split()
+def updating_line(line):
+    num = line.split()
+    print(int(num[-1]) + 10)
 
-print(int(user_input[-1]) + 10)
+lines = [
+    "результат операции: 42",
+    "результат операции: 54",
+    "результат работы программы: 209",
+    "результат: 2"
+]
+
+for line in lines:
+    updating_line(line)
