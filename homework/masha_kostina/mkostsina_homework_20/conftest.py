8a301bd82633c0862bf05e9e12c291badacb77cb
MashaKostsina <kostsinamaryia@gmail.com>
2025-09-21 21:03:35 +0300

    homework 20 is done

---
+++ homework/masha_kostina/mkostsina_homework_20/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+def pytest_configure(config):
+    """Регистрирует пользовательские маркеры для pytest"""
+    config.addinivalue_line("markers", "medium: marks tests as medium priority")
+    config.addinivalue_line("markers", "critical: marks tests as critical priority")
