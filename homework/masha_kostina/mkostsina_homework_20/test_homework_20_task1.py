2649bfff3ef68e29439b6aa211254e25207c4a1f
MashaKostsina <kostsinamaryia@gmail.com>
2025-09-30 21:51:47 +0300

    Homework 21 is done. there are changes in homework 20

--- homework/masha_kostina/mkostsina_homework_20/test_homework_20_task1.py
+++ homework/masha_kostina/mkostsina_homework_20/test_homework_20_task1.py
@@ -1,76 +1,116 @@
 import pytest
 import requests
+import allure
 
 
-@pytest.fixture(scope="session", autouse=True)
-def start_and_finish():
-    print("\nStart testing")
-    yield
-    print("\nTesting completed")
-
-
-@pytest.fixture(autouse=True)
-def before_after_each_test():
-    print("before test")
-    yield
-    print("after test")
-
-
-@pytest.fixture()
-def new_obj_id():
-    body = {"name": "New object", "data": {'color': 'pink', 'size': 'medium'}}
-    response = requests.post("http://objapi.course.qa-practice.com/object", json=body)
-    obj_id = response.json()['id']
-    yield obj_id
-    requests.delete(f"http://objapi.course.qa-practice.com/object/{obj_id}")
-
-
+@allure.feature("Objects")
+@allure.story("Get objects")
+@allure.tag("smoke")
+@allure.title("Получение всех объектов")
+@allure.testcase("https://google.com/", "Test case 1")
+@allure.issue("https://google.com/", "Bug 003")
 @pytest.mark.medium
-def test_get_all_obj():
-    response = requests.get("http://objapi.course.qa-practice.com/object").json()
-    assert len(response) == 1, 'There are extra objects'
+def test_get_all_obj(attach_response):
+    with allure.step("Run get request for all objects"):
+        response = requests.get("http://objapi.course.qa-practice.com/object")
+        attach_response(response, name="Get all objects")
+        data = response.json()
+
+    with allure.step("Check number of objects"):
+        assert len(data) == 2, 'There are extra objects'
 
 
+@allure.feature("Objects")
+@allure.story("Get objects")
+@allure.tag("smoke")
+@allure.title("Получение объекта по id")
 @pytest.mark.critical
-def test_get_obj_by_id(new_obj_id):
-    response = requests.get(f"http://objapi.course.qa-practice.com/object/{new_obj_id}").json()
-    assert response['id'] == new_obj_id
+def test_get_obj_by_id(new_obj_id, attach_response):
+    with allure.step(f'Run request for object by id - {new_obj_id}'):
+        response = attach_response(
+            requests.get(f"http://objapi.course.qa-practice.com/object/{new_obj_id}"),
+            name="Get object by id"
+        )
+    with allure.step(f'Check response id with {new_obj_id}'):
+        assert response['id'] == new_obj_id
 
 
-@pytest.mark.parametrize('body', [{"name": "New object1", "data": {'color': 'pink', 'size': 'large'}},
-                                  {"name": "New object2", "data": {'color': 'pink', 'size': 'small'}},
-                                  {"name": "New object3", "data": {'color': 'red', 'size': 'medium'}}])
-def test_create_obj(body, new_obj_id):
-    response = requests.post("http://objapi.course.qa-practice.com/object", json=body)
-    assert response.status_code == 200, 'Status code is incorrect'
-    assert response.json()['id'] == new_obj_id + 1, 'ID is incorrect'
+@allure.feature("Objects")
+@allure.story("Create objects")
+@allure.tag("smoke")
+@allure.title("Создание объекта")
+@pytest.mark.parametrize('body', [
+    {"name": "New object1", "data": {'color': 'pink', 'size': 'large'}},
+    {"name": "New object2", "data": {'color': 'pink', 'size': 'small'}},
+    {"name": "New object3", "data": {'color': 'red', 'size': 'medium'}}
+])
+def test_create_obj(body, new_obj_id, attach_response):
+    with allure.step(f'Create 3 different objects'):
+        response = attach_response(
+            requests.post("http://objapi.course.qa-practice.com/object", json=body),
+            name="Create object"
+        )
+    with allure.step("Check response's status code"):
+        assert requests.post("http://objapi.course.qa-practice.com/object", json=body).status_code == 200, \
+            'Status code is incorrect'
+    with allure.step(f'Check id in response'):
+        assert response['id'] == new_obj_id + 1, 'ID is incorrect'
 
 
-def test_put_obj(new_obj_id):
+@allure.feature("Test")
+@allure.story("Update objects")
+@allure.title("Обновление объекта put")
+def test_put_obj(new_obj_id, attach_response):
     body = {
         "name": "New object UPD",
         "data": {'color': 'red', 'size': 'large'}
     }
-    response = requests.put(f"http://objapi.course.qa-practice.com/object/{new_obj_id}", json=body).json()
-    assert response['name'] == "New object UPD"
-    assert response['data']['color'] == 'red'
-    assert response['data']['size'] == 'large'
+    with allure.step(f'Update an object by a {body}'):
+        response = attach_response(
+            requests.put(f"http://objapi.course.qa-practice.com/object/{new_obj_id}", json=body),
+            name="Update object PUT"
+        )
+    with allure.step("Check name in response"):
+        assert response['name'] == "New object UPD"
+    with allure.step("Check color in response"):
+        assert response['data']['color'] == 'red'
+    with allure.step("Check size in response"):
+        assert response['data']['size'] == 'large'
 
 
-def test_patch_obj(new_obj_id):
+@allure.feature("Test")
+@allure.story("Update objects")
+@allure.title("Обновление объекта patch")
+def test_patch_obj(new_obj_id, attach_response):
     body = {
         "name": "New object",
         "data": {'color': 'red', 'size': 'medium'}
     }
-    response = requests.patch(f"http://objapi.course.qa-practice.com/object/{new_obj_id}", json=body).json()
-    assert response['name'] == "New object"
-    assert response['data']['color'] == 'red'
-    assert response['data']['size'] == 'medium'
+    with allure.step(f'Update an object by a {body}'):
+        response = attach_response(
+            requests.patch(f"http://objapi.course.qa-practice.com/object/{new_obj_id}", json=body),
+            name="Update object PATCH"
+        )
+    with allure.step("Check name in response"):
+        assert response['name'] == "New object"
+    with allure.step("Check color in response"):
+        assert response['data']['color'] == 'red'
+    with allure.step("Check size in response"):
+        assert response['data']['size'] == 'medium'
 
 
-def test_delete_obj(new_obj_id):
-    delete_response = requests.delete(f"http://objapi.course.qa-practice.com/object/{new_obj_id}")
-    assert delete_response.status_code == 200
+@allure.feature("Test2")
+@allure.story("Delete objects")
+@allure.title("Удаление объекта")
+def test_delete_obj(new_obj_id, attach_response):
+    with allure.step(f'Run request to delete the object with id {new_obj_id}'):
+        delete_response = requests.delete(f"http://objapi.course.qa-practice.com/object/{new_obj_id}")
+        attach_response(delete_response, name="Delete object")
+    with allure.step("Check response's status code"):
+        assert delete_response.status_code == 200
 
-    get_response = requests.get(f"http://objapi.course.qa-practice.com/object/{new_obj_id}")
-    assert get_response.status_code == 404
+    with allure.step(f'Run request to get the object with id {new_obj_id} after deleting'):
+        get_response = requests.get(f"http://objapi.course.qa-practice.com/object/{new_obj_id}")
+        attach_response(get_response, name="Get deleted object")
+    with allure.step("Check response's status code"):
+        assert get_response.status_code == 404
